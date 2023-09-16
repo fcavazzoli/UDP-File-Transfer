f715f587d5fffeea03beff407615599b3f5495bd
AlePaff <43119891+AlePaff@users.noreply.github.com>
2023-09-16 10:43:12 -0300

    feat: ✨ implement logger (#2)
    * parser para la descarga
    se usó la lib de python "argparse"
    
    * readme basico para los comandos
    
    * parser de upload y start-server
    tambien se explicita la accion "store" que hace el parser
    
    * readme sobre como ejecutar actualmente
    
    * más manija, solo deja verbose o quiet, no ambos
    
    * valores default y constantes
    
    * TODO que me olvidé eliminar del parser
    
    * configuración del logger
    
    * inicializacion del logger para cada comando
    
    ---------
    
    Co-authored-by: Federico Cavazzoli <fedecavazzoli@gmail.com>

--- src/modules/constants.py
+++ src/modules/constants.py
@@ -7,4 +7,9 @@ DEFAULT_RELIABLE_PROTOCOL = "stop-and-wait"
 
 # file paths default ???
 DEFAULT_STORAGE_DIR = "./storage/"
+
 DEFAULT_FILE_NAME = "file.txt"
+
+# log file name
+DEFAULT_LOG_FILE_NAME = "app.log"
+
