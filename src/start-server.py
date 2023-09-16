07a3217ae53ae76662921ff51025aeb45d863527
AlePaff <43119891+AlePaff@users.noreply.github.com>
2023-09-16 10:38:27 -0300

    Parseo de comandos (#1)
    * parser para la descarga
    se usó la lib de python "argparse"
    
    * readme basico para los comandos
    
    * parser de upload y start-server
    tambien se explicita la accion "store" que hace el parser
    
    * readme sobre como ejecutar actualmente
    
    * más manija, solo deja verbose o quiet, no ambos
    
    * valores default y constantes

---
+++ src/start-server.py
@@ -0,0 +1,9 @@
+from modules.parser import parse_server_args
+
+
+if __name__ == "__main__":
+    parsed_args = parse_server_args()
+    
+    #TODO: logger
+    #TODO: seleccionar protocolo
+    #TODO: server stuff (?
