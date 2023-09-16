66919e021d1a35c0382fd1156b060d8469c5bf25
Federico <fedecavazzoli@gmail.com>
2023-09-16 13:39:34 -0300

    feat: add dummy logger as default

--- src/lib/helpers/network_builder.py
+++ src/lib/helpers/network_builder.py
@@ -1,9 +1,10 @@
+from lib.common.logger_setup import DummyLogger
 from lib.constants import DEFAULT_IP, DEFAULT_PORT
 
 class NetworkBuilder:
     def __init__(self, type):
         self.type = type
-        self.logger = None
+        self.logger = DummyLogger()
         self.port = DEFAULT_PORT
         self.host = DEFAULT_IP
 
