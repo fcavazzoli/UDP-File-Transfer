af8951248d3d6324b7212d88f3bc93d8ea00e0a9
Federico <fedecavazzoli@gmail.com>
2023-09-16 13:28:30 -0300

    fix: use default ip for host instead of port

--- src/lib/helpers/network_builder.py
+++ src/lib/helpers/network_builder.py
@@ -1,11 +1,11 @@
-from lib.constants import DEFAULT_PORT
+from lib.constants import DEFAULT_IP, DEFAULT_PORT
 
 class NetworkBuilder:
     def __init__(self, type):
         self.type = type
         self.logger = None
         self.port = DEFAULT_PORT
-        self.host = DEFAULT_PORT
+        self.host = DEFAULT_IP
 
     def set_logger(self, logger):
         self.logger = logger
