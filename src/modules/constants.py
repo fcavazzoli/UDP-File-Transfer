bce42f393ff1e5998ef8e4169b4588cff9add962
AlePaff <alejandropaff@gmail.com>
2023-09-16 01:50:48 -0300

    valores default y constantes

---
+++ src/modules/constants.py
@@ -0,0 +1,10 @@
+# direcciones default
+DEFAULT_IP = "localhost"
+DEFAULT_PORT = 5000
+
+# protocolos default
+DEFAULT_RELIABLE_PROTOCOL = "stop-and-wait"
+
+# file paths default ???
+DEFAULT_STORAGE_DIR = "./storage/"
+DEFAULT_FILE_NAME = "file.txt"
--- src/modules/parser.py
+++ src/modules/parser.py
@@ -1,7 +1,10 @@
 import argparse
+from modules.constants import DEFAULT_PORT, DEFAULT_IP, DEFAULT_RELIABLE_PROTOCOL
 
 def build_base_parser(command_desc):
-    """Contruye el parser con los argumentos comunes a todos los comandos: download, upload y start-server"""
+    """Contruye el parser con los argumentos comunes a todos los comandos: download, upload y start-server
+     - `command_desc`: descripcion del comando que se muestra en el help
+    """
 
     parser = argparse.ArgumentParser(description=command_desc)
 
@@ -17,20 +20,20 @@ def build_base_parser(command_desc):
     group.add_argument( "-v", "--verbose", action="store_true", help="increase output verbosity")
     group.add_argument( "-q", "--quiet", action="store_true", help="decrease output verbosity")
 
-    parser.add_argument("-H", "--host", action="store", metavar="addr", help="server IP address")
-    parser.add_argument("-p", "--port", action="store", metavar="port", help="server port")
+    parser.add_argument("-H", "--host", action="store", metavar="ADDR", help="server IP address", default=DEFAULT_IP)
+    parser.add_argument("-p", "--port", action="store", metavar="PORT", help="server port", default=DEFAULT_PORT)
 
     # Tipo de protocolo: Stop and Wait o Selective Repeat
     # Es un nuevo argumento, no está explicitamente en el enunciado
-    parser.add_argument("-P", "--protocol", metavar="protocol", help="server/client protocol")
+    parser.add_argument("-P", "--protocol", metavar="PROTOCOL", help="server/client protocol", default=DEFAULT_RELIABLE_PROTOCOL)
 
     return parser
 
 def parse_download_args():
     """Parsea los argumentos para el comando download y devuelve un objeto con los argumentos parseados"""
     parser = build_base_parser("Downloads a file from a server")
-    parser.add_argument("-d", "--dst", action="store", metavar="filepath", help="destination file path")
-    parser.add_argument("-n", "--name", action="store", metavar="filename", help="file name") #, required=True)
+    parser.add_argument("-d", "--dst", action="store", metavar="FILEPATH", help="destination file path")
+    parser.add_argument("-n", "--name", action="store", metavar="FILENAME", help="file name") #, required=True)
 
     parsed_args = parser.parse_args()
     return parsed_args
@@ -39,8 +42,8 @@ def parse_download_args():
 def parse_upload_args():
     """Parsea los argumentos para el comando upload y devuelve un objeto con los argumentos parseados"""
     parser = build_base_parser("Uploads a file to a server")
-    parser.add_argument("-s", "--src", action="store", metavar="filepath", help="source file path")
-    parser.add_argument("-n", "--name", action="store", metavar="filename", help="file name") #, required=True)
+    parser.add_argument("-s", "--src", action="store", metavar="FILEPATH", help="source file path")
+    parser.add_argument("-n", "--name", action="store", metavar="FILENAME", help="file name") #, required=True)
     # NOTE: poner un nombre default al archivo o ponerlo siempre como requerido?
 
     parsed_args = parser.parse_args()
@@ -49,7 +52,7 @@ def parse_upload_args():
 def parse_server_args():
     """Parsea los argumentos para el comando start-server y devuelve un objeto con los argumentos parseados"""
     parser = build_base_parser("Starts a server that accepts file uploads and downloads")
-    parser.add_argument("-s", "--storage", action="store", metavar="dirpath", help="storage dir path")
+    parser.add_argument("-s", "--storage", action="store", metavar="DIRPATH", help="storage dir path")
 
     parsed_args = parser.parse_args()
     return parsed_args
