4f64aa609d523ccb26b47a7ebe2a52c1d10cfcb3
AlePaff <alejandropaff@gmail.com>
2023-09-16 02:56:49 -0300

    inicializacion del logger para cada comando

--- src/start-server.py
+++ src/start-server.py
@@ -1,8 +1,20 @@
 from modules.parser import parse_server_args
-
+from modules.logger_setup import logger_setup
 
 if __name__ == "__main__":
     parsed_args = parse_server_args()
+    # acceder a los argumentos parseados
+    print(parsed_args)
+    print(parsed_args.host)
+    print(parsed_args.verbose)
+    
+    logger = logger_setup(parsed_args)
+    # probar con -v y -q para ver los distintos niveles de log
+    logger.critical("Esto es critico")
+    logger.error("Es solo un error")
+    logger.warning("Warning")
+    logger.info("Te voy contando")
+    logger.debug("Te cuento con mucho detalle")
     
     #TODO: logger
     #TODO: seleccionar protocolo
