5b58f68b5d5d61563f63c914c46d37d0f8c62158
Federico Cavazzoli <fedecavazzoli@gmail.com>
2023-09-30 16:00:04 -0300

    feat: ⚡ file handler (#15)
    * feat: add read_bytes file
    
    * feat: add write file in binary
    
    * lint fix

--- mininet/create_topology_from_scratch.py
+++ mininet/create_topology_from_scratch.py
@@ -6,6 +6,7 @@ ARGUMENTS_AMOUNT = 3
 PACKAGE_LOSS_PATH = "package_loss.txt"
 HOSTS_AMOUT_PATH = "amount_hosts.txt"
 
+
 def append_logs(log, path):
     try:
         with open(path, "r") as f:
@@ -18,11 +19,13 @@ def append_logs(log, path):
         with open(path, "w") as f:
             f.write(log)
 
+
 def create_logs(package_loss, hosts_amount):
     print(f"La probabilidad de la perdida de paquetes será: {package_loss}")
     print(f"El número de hosts será: {hosts_amount}")
     append_logs(str(hosts_amount), HOSTS_AMOUT_PATH)
     append_logs(str(package_loss), PACKAGE_LOSS_PATH)
+
 
 if len(sys.argv) != ARGUMENTS_AMOUNT:
     print("Por favor ingresa como primer parámetro la cantidad de hosts (entre 2 y 10) y como segundo parámetro, el     porcentaje de package loss (entre 0 y 100.")
@@ -31,7 +34,7 @@ if len(sys.argv) != ARGUMENTS_AMOUNT:
 else:
     hosts_parameter = sys.argv[1]
     package_loss_parameter = sys.argv[2]
-    
+
     if hosts_parameter.isdigit() and package_loss_parameter.isdigit():
         package_loss = int(package_loss_parameter)
         hosts_amount = int(hosts_parameter)
@@ -41,10 +44,8 @@ else:
                 mn_command = ["sudo", "mn", "--custom", "topology.py", "--topo", "project"]
                 subprocess.run(mn_command)
             else:
-                print("Error: el primer parámetro (cantidad de hosts) tiene que estar entre 2 y 10.")  
+                print("Error: el primer parámetro (cantidad de hosts) tiene que estar entre 2 y 10.")
         else:
             print("Error: el segundo parámetro (procentaje de la pérdida de paquetes) tiene que estar entre 0 y 100.")
-    else: 
+    else:
         print("Error: Todos los inputs deben ser números.")
-
-
---
+++ src/lib/common/file_handler.py
@@ -0,0 +1,38 @@
+import logging
+
+
+class FileHandler:
+    def __init__(self, file_path, logger: logging.Logger):
+        self.file_path = file_path
+        self.logger = logger
+
+    def read_bytes(self, chunk_size=None):
+        try:
+            with open(self.file_path, 'rb') as file:
+                if chunk_size is None:
+                    file_bytes = file.read()
+                else:
+                    file_bytes = bytearray()
+                    while True:
+                        chunk = file.read(chunk_size)
+                        if not chunk:
+                            break
+                        file_bytes.extend(chunk)
+                return file_bytes
+        except FileNotFoundError:
+            self.logger.error(f'File {self.file_path} not found')
+            return None
+        except PermissionError:
+            self.logger.error(f'Permission denied for file {self.file_path}')
+            return None
+
+    def write_bytes(self, file_bytes):
+        try:
+            with open(self.file_path, 'wb') as file:
+                file.write(file_bytes)
+        except FileNotFoundError:
+            self.logger.error(f'File {self.file_path} not found')
+            return None
+        except PermissionError:
+            self.logger.error(f'Permission denied for file {self.file_path}')
+            return None
