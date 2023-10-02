c1a32dfa538a6f8dcf15479ccbc34935ce8351a9
Ale Acevedo <alejo.acevedo@deel.com>
2023-10-02 20:36:47 -0300

    use connection class on both ends (#26)

--- src/lib/client/__init__.py
+++ src/lib/client/__init__.py
@@ -1,27 +1,18 @@
-from ..common.rdt_managers import RDTManagers
-from ..common.socket import Socket
-from ..common.configs import SingletonConfiguration
+from lib.common.connection import Connection
 
 
 class Client:
     def __init__(self, remote_ip, remote_port, logger):
-        self.socket = Socket((remote_ip, remote_port))
+        self.connection = Connection()
+        self.address = (remote_ip, remote_port)
         self.logger = logger
 
-    def _send(self, message):
-        self.socket.send(message)
-
-    def _receive(self):
-        return self.socket.recv()
-
     def connect(self):
-        self._send(bytes('handshake', "utf-8"))
-        data, addr = self._receive()
-        self.socket.change_destination(addr)
-        self.socket.listen()
-        rdt_type = SingletonConfiguration().get('protocol')
-        self.sender_handler = RDTManagers.get_sender_handler(rdt_type, self.socket)
+        self.connection.connect(self.address)
+        self.logger.info('Connected to server')
 
     def send(self, message):
-        print('sending %s' %message)
-        self.sender_handler.send(message)
+        self.connection.send(message)
+
+    def recv(self):
+        return self.connection.recv()
---
+++ src/lib/common/connection.py
@@ -0,0 +1,35 @@
+from .rdt_managers import RDTManagers
+from .socket import Socket
+from .configs import SingletonConfiguration
+
+
+class Connection:
+    socket = None
+    receiverHandler = None
+
+    def __init__(self):
+        self.rdt_type = SingletonConfiguration().get('protocol')
+
+    def connect(self, address):
+        self.socket = Socket(address)
+        self.socket.send(b'handshake')
+        recv, addr = self.socket.recv()
+        self.socket.change_destination(addr)
+        self.socket.listen()
+        self.config_protocol()
+
+    def accept(self, address):
+        self.socket = Socket(address)
+        self.socket.send(b'handshake Received')
+        self.socket.listen()
+        self.config_protocol()
+
+    def send(self, message):
+        self.senderHandler.send(message)
+
+    def recv(self):
+        return self.receiverHandler.recv()
+
+    def config_protocol(self):
+        self.receiverHandler = RDTManagers.get_receiver_handler(self.rdt_type, self.socket)
+        self.senderHandler = RDTManagers.get_sender_handler(self.rdt_type, self.socket)
