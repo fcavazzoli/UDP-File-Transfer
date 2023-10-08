eefd3ba6148648c17e162a16aeceb178fb41900e
Agustin D. Palmeira <agustinpalmeira@gmail.com>
2023-10-08 17:53:09 -0300

    Logs added and fix issue while adding hosts.

--- mininet/topology.py
+++ mininet/topology.py
@@ -21,19 +21,22 @@ class Project(Topo):
         package_loss = get_first_value_from_file(PACKAGE_LOSS_PATH)
 
         # Add hosts
+        # First host should act as server.
         for i in range(1, num_hosts + 1):
             host = self.addHost("h{0}".format(i), ip="10.0.0.{0}".format(i))
 
         # Add switch
+        print("Adding switch s1..")
         s1 = self.addSwitch('s1')
 
-        # Add links from hosts to switch without specifying loss
+        # Add links from hosts to switch.
+        # Add package loss to the first host only regarding on package_loss value.
         for i in range(1, num_hosts + 1):
-            self.addLink("h{0}".format(i), s1, cls=TCLink)
-
-        # Add package loss to the first host's interface.
-        # First host should act as server.
-        h1_intf = s1.intf('s1-eth1')
-        h1_intf.config(loss=package_loss)
-
+            if i == 1:
+            	print("Adding server h{0}..".format(i))
+            	self.addLink("h{0}".format(i), s1, cls=TCLink, loss=package_loss)
+            else:
+            	print("Adding host h{0}..".format(i))
+            	self.addLink("h{0}".format(i), s1, cls=TCLink)
+            	
 topos = {'project': (lambda: Project())}
