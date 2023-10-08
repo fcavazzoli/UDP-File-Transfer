7aef894123366e7ffe78e9829b42897feb774c31
Agustin Daniel Palmeira <agustinpalmeira@gmail.com>
2023-10-08 01:30:39 -0300

    Refactor rdt_managers.

--- src/lib/common/rdt_managers/__init__.py
+++ src/lib/common/rdt_managers/__init__.py
@@ -3,25 +3,36 @@ from .selective_repeat.sender_handler import SenderHandler as SRSenderHandler
 from .stop_and_wait.receiver_handler import ReceiverHandler as SWReceiverHandler
 from .stop_and_wait.sender_handler import SenderHandler as SWSenderHandler
 
-
 class RDTManagers:
+    @staticmethod
     def get_manager(type, socket):
-        if (type == 'selective-repeat'):
-            return SRReceiverHandler(socket), SRSenderHandler(socket)
-        elif (type == 'stop-and-wait'):
-            return SWReceiverHandler(socket), SWSenderHandler(socket)
+        handler_map = {
+            'selective-repeat': (SRReceiverHandler, SRSenderHandler),
+            'stop-and-wait': (SWReceiverHandler, SWSenderHandler),
+        }
+        receiver_handler, sender_handler = handler_map.get(type, (None, None))
+        if receiver_handler and sender_handler:
+            return receiver_handler(socket), sender_handler(socket)
         raise Exception('Invalid type')
 
+    @staticmethod
     def get_receiver_handler(type, socket):
-        if (type == 'selective-repeat'):
-            return SRReceiverHandler(socket)
-        elif (type == 'stop-and-wait'):
-            return SWReceiverHandler(socket)
+        handler_map = {
+            'selective-repeat': SRReceiverHandler,
+            'stop-and-wait': SWReceiverHandler,
+        }
+        receiver_handler = handler_map.get(type)
+        if receiver_handler:
+            return receiver_handler(socket)
         raise Exception('Invalid type')
 
+    @staticmethod
     def get_sender_handler(type, socket):
-        if (type == 'selective-repeat'):
-            return SRSenderHandler(socket)
-        elif (type == 'stop-and-wait'):
-            return SWSenderHandler(socket)
+        handler_map = {
+            'selective-repeat': SRSenderHandler,
+            'stop-and-wait': SWSenderHandler,
+        }
+        sender_handler = handler_map.get(type)
+        if sender_handler:
+            return sender_handler(socket)
         raise Exception('Invalid type')
