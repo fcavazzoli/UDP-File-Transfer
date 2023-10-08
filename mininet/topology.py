bdac75c237ea1111d0e330dc1015025e1db7fe23
Agustin Daniel Palmeira <agustinpalmeira@gmail.com>
2023-10-08 04:10:09 -0300

    Package loss on first host only.

--- mininet/topology.py
+++ mininet/topology.py
@@ -3,8 +3,7 @@ from mininet.link import TCLink
 import sys
 
 PACKAGE_LOSS_PATH = "package_loss.txt"
-HOSTS_AMOUT_PATH = "amount_hosts.txt"
-
+HOSTS_AMOUNT_PATH = "amount_hosts.txt"
 
 def get_first_value_from_file(path):
     try:
@@ -13,13 +12,12 @@ def get_first_value_from_file(path):
     except FileNotFoundError:
         print("Error: El archivo ha sido eliminado durante la ejecución. Vuelva a intentar.")
 
-
 class Project(Topo):
     def __init__(self):
         # Initialize topology
         Topo.__init__(self)
 
-        num_hosts = get_first_value_from_file(HOSTS_AMOUT_PATH)
+        num_hosts = get_first_value_from_file(HOSTS_AMOUNT_PATH)
         package_loss = get_first_value_from_file(PACKAGE_LOSS_PATH)
 
         # Add hosts
@@ -29,9 +27,13 @@ class Project(Topo):
         # Add switch
         s1 = self.addSwitch('s1')
 
-        # Add links
+        # Add links from hosts to switch without specifying loss
         for i in range(1, num_hosts + 1):
-            self.addLink("h{0}".format(i), s1, cls=TCLink, loss=package_loss)
+            self.addLink("h{0}".format(i), s1, cls=TCLink)
 
+        # Add package loss to the first host's interface.
+        # First host should act as server.
+        h1_intf = s1.intf('s1-eth1')
+        h1_intf.config(loss=package_loss)
 
 topos = {'project': (lambda: Project())}
