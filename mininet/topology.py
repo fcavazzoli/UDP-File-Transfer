381f34c9ba564c9eb08afbd1cc65b0866e19ddf4
Agustin Palmeira <agustinpalmeira@gmail.com>
2023-10-09 00:51:15 -0300

    Package loss on first host only. (#38)
    * Package loss on first host only.
    
    * Logs added and fix issue while adding hosts.

--- mininet/topology.py
+++ mininet/topology.py
@@ -3,8 +3,7 @@ from mininet.link import TCLink
 import sys
 
 PACKAGE_LOSS_PATH = "package_loss.txt"
-HOSTS_AMOUT_PATH = "amount_hosts.txt"
-
+HOSTS_AMOUNT_PATH = "amount_hosts.txt"
 
 def get_first_value_from_file(path):
     try:
@@ -13,25 +12,31 @@ def get_first_value_from_file(path):
     except FileNotFoundError:
         print("Error: El archivo ha sido eliminado durante la ejecución. Vuelva a intentar.")
 
-
 class Project(Topo):
     def __init__(self):
         # Initialize topology
         Topo.__init__(self)
 
-        num_hosts = get_first_value_from_file(HOSTS_AMOUT_PATH)
+        num_hosts = get_first_value_from_file(HOSTS_AMOUNT_PATH)
         package_loss = get_first_value_from_file(PACKAGE_LOSS_PATH)
 
         # Add hosts
+        # First host should act as server.
         for i in range(1, num_hosts + 1):
             host = self.addHost("h{0}".format(i), ip="10.0.0.{0}".format(i))
 
         # Add switch
+        print("Adding switch s1..")
         s1 = self.addSwitch('s1')
 
-        # Add links
+        # Add links from hosts to switch.
+        # Add package loss to the first host only regarding on package_loss value.
         for i in range(1, num_hosts + 1):
-            self.addLink("h{0}".format(i), s1, cls=TCLink, loss=package_loss)
-
-
+            if i == 1:
+            	print("Adding server h{0}..".format(i))
+            	self.addLink("h{0}".format(i), s1, cls=TCLink, loss=package_loss)
+            else:
+            	print("Adding host h{0}..".format(i))
+            	self.addLink("h{0}".format(i), s1, cls=TCLink)
+            	
 topos = {'project': (lambda: Project())}
